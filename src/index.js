4a72f5337ac62927bd3a9db13a04364d1dc03158
Kibuchi3560 <jontezbuchiki3560@gmail.com>
2024-11-21 11:58:29 +0300

    Updates

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Home />);
